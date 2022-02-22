e8d67f648b1baafc4c023de5bed82d648190f9b6
MichaelWilliam Giannini <michaelwilliam.giann@mymail.champlain.edu>
2022-02-21 23:29:05 -0500

    Convert 2 space indents to 4 spaces

--- pa3/sortingFunctions.h
+++ pa3/sortingFunctions.h
@@ -26,20 +26,20 @@ Post: list is sorted in ascending order
 template <typename T>
 void bubbleSort(T list[], int size)
 {
-  bool swapped = true;
-  int i;
-  while(swapped)
-  {
-    swapped = false;
-    for(i = 0; i < size - 1; i++)
+    bool swapped = true;
+    int i;
+    while (swapped)
     {
-      if(list[i+1] < list[i])
-      {
-        std::swap(list[i+1], list[i]);
-        swapped = true;
-      }
+        swapped = false;
+        for (i = 0; i < size - 1; i++)
+        {
+            if (list[i + 1] < list[i])
+            {
+                std::swap(list[i + 1], list[i]);
+                swapped = true;
+            }
+        }
     }
-  }
 }
 
 /* Sort an array using the gnome sort algorithm
@@ -53,18 +53,18 @@ Post: list is sorted in ascending order
 template <typename T>
 void gnomeSort(T list[], int size)
 {
-  int i = 0;
+    int i = 0;
 
-  while(i < size)
-  {
-    if(i == 0 || list[i] >= list[i - 1])
-      i++;
-    else
+    while (i < size)
     {
-      std::swap(list[i], list[i - 1]);
-      i--;
+        if (i == 0 || list[i] >= list[i - 1])
+            i++;
+        else
+        {
+            std::swap(list[i], list[i - 1]);
+            i--;
+        }
     }
-  }
 }
 
 /* Sort an array using the insertion sort algorithm
@@ -75,18 +75,18 @@ Post: list is sorted in ascending order
 template <typename T>
 void insertionSort(T list[], int size)
 {
-  int value, i, j;
-  for(i = 1; i < size; i++)
-  {
-    value = list[i];
-    j = i - 1;
-    while(j >= 0 && list[j] > value)
+    int value, i, j;
+    for (i = 1; i < size; i++)
     {
-      list[j+1] = list[j];
-      j--;
+        value = list[i];
+        j = i - 1;
+        while (j >= 0 && list[j] > value)
+        {
+            list[j + 1] = list[j];
+            j--;
+        }
+        list[j + 1] = value;
     }
-    list[j+1] = value;
-  }
 }
 
 /* Merge two parts of a list in a sorted manner
@@ -94,56 +94,56 @@ void insertionSort(T list[], int size)
 Pre: list, inclusive bounds, and midpoint provided
 Post: list is merged in a sorted manner between the bounds
 */
-template <typename T> 
+template <typename T>
 void merge(T list[], int lowerBound, int mid, int upperBound)
 {
-  int size1, size2, i, j, k;
+    int size1, size2, i, j, k;
 
-  size1 = mid - lowerBound + 1;
-  size2 = upperBound - mid;
+    size1 = mid - lowerBound + 1;
+    size2 = upperBound - mid;
 
-  T* tmp1 = new T[size1];
-  T* tmp2 = new T[size2];
+    T *tmp1 = new T[size1];
+    T *tmp2 = new T[size2];
 
-  for(i = 0; i < size1; i++)
-    tmp1[i] = list[lowerBound + i];
+    for (i = 0; i < size1; i++)
+        tmp1[i] = list[lowerBound + i];
 
-  for(j = 0; j < size2; j++)
-    tmp2[j] = list[mid + j + 1];
+    for (j = 0; j < size2; j++)
+        tmp2[j] = list[mid + j + 1];
 
-  i = 0;
-  j = i;
+    i = 0;
+    j = i;
 
-  for(k = lowerBound; k < upperBound && i < size1 && j < size2; k++)
-  {
-    if(tmp1[i] <= tmp2[j])
+    for (k = lowerBound; k < upperBound && i < size1 && j < size2; k++)
     {
-      list[k] = tmp1[i];
-      i++;
+        if (tmp1[i] <= tmp2[j])
+        {
+            list[k] = tmp1[i];
+            i++;
+        }
+        else
+        {
+            list[k] = tmp2[j];
+            j++;
+        }
     }
-    else
+
+    while (i < size1)
     {
-      list[k] = tmp2[j];
-      j++;
+        list[k] = tmp1[i];
+        i++;
+        k++;
     }
-  }
 
-  while(i < size1)
-  {
-    list[k] = tmp1[i];
-    i++;
-    k++;
-  }
+    while (j < size2)
+    {
+        list[k] = tmp2[j];
+        j++;
+        k++;
+    }
 
-  while(j < size2)
-  {
-    list[k] = tmp2[j];
-    j++;
-    k++;
-  }
-
-  delete [] tmp1;
-  delete [] tmp2;
+    delete[] tmp1;
+    delete[] tmp2;
 }
 
 /* Sort an array using the merge sort algorithm
@@ -151,18 +151,18 @@ void merge(T list[], int lowerBound, int mid, int upperBound)
 Pre: list and inclusive bounds provided
 Post: list is sorted in ascending order between the bounds
 */
-template <typename T> 
+template <typename T>
 void mergeSort(T list[], int lowerBound, int upperBound)
 {
-  int mid;
+    int mid;
 
-  if(lowerBound < upperBound)
-  {
-    mid = (lowerBound + upperBound) / 2;
-    mergeSort(list, lowerBound, mid);
-    mergeSort(list, mid + 1, upperBound);
-    merge(list, lowerBound, mid, upperBound);
-  }
+    if (lowerBound < upperBound)
+    {
+        mid = (lowerBound + upperBound) / 2;
+        mergeSort(list, lowerBound, mid);
+        mergeSort(list, mid + 1, upperBound);
+        merge(list, lowerBound, mid, upperBound);
+    }
 }
 
 /* Sort an array using the quick sort algorithm
@@ -170,35 +170,35 @@ void mergeSort(T list[], int lowerBound, int upperBound)
 Pre: list and inclusive bounds provided
 Post: list is sorted in ascending order between the bounds
 */
-template <typename T> 
+template <typename T>
 void quickSort(T list[], int lowerBound, int upperBound)
 {
-  int pivot, i, j;
+    int pivot, i, j;
 
-  i = lowerBound;
-  j = upperBound;
+    i = lowerBound;
+    j = upperBound;
 
-  pivot = list[(lowerBound + upperBound) / 2];
+    pivot = list[(lowerBound + upperBound) / 2];
 
-  while(i <= j)
-  {
-    while(list[i] < pivot)
-      i++;
-    while(list[j] > pivot)
-      j--;
-
-    if(i <= j)
+    while (i <= j)
     {
-      std::swap(list[i], list[j]);
-      i++;
-      j--;
-    }
-  }
+        while (list[i] < pivot)
+            i++;
+        while (list[j] > pivot)
+            j--;
 
-  if(lowerBound < j)
-    quickSort(list, lowerBound, j);
-  if(i < upperBound)
-    quickSort(list, i, upperBound);
+        if (i <= j)
+        {
+            std::swap(list[i], list[j]);
+            i++;
+            j--;
+        }
+    }
+
+    if (lowerBound < j)
+        quickSort(list, lowerBound, j);
+    if (i < upperBound)
+        quickSort(list, i, upperBound);
 }
 
 /* Sort an array using the selection sort algorithm
@@ -209,21 +209,21 @@ Post: list is sorted in ascending order
 template <typename T>
 void selectionSort(T list[], int size)
 {
-  int minIndex, i, j;
+    int minIndex, i, j;
 
-  for(i = 0; i < size - 1; i++)
-  {
-    minIndex = i;
-
-    for(j = i + 1; j < size; j++)
+    for (i = 0; i < size - 1; i++)
     {
-      if(list[j] < list[minIndex])
-        minIndex = j;
-    }
+        minIndex = i;
 
-    if(minIndex != i)
-      std::swap(list[i], list[minIndex]);
-  }
+        for (j = i + 1; j < size; j++)
+        {
+            if (list[j] < list[minIndex])
+                minIndex = j;
+        }
+
+        if (minIndex != i)
+            std::swap(list[i], list[minIndex]);
+    }
 }
 
 /* Sort an array using the shell sort algorithm
@@ -234,24 +234,24 @@ Post: list is sorted in ascending order
 template <typename T>
 void shellSort(T list[], int size)
 {
-  int gap, i;
+    int gap, i;
 
-  gap = size / 2;
+    gap = size / 2;
 
-  while(gap > 0)
-  {
-    for(i = 0; i < size - gap; i++)
+    while (gap > 0)
     {
-      if(list[i] > list[i + gap])
-      {
-        std::swap(list[i], list[i + gap]);
-      }
+        for (i = 0; i < size - gap; i++)
+        {
+            if (list[i] > list[i + gap])
+            {
+                std::swap(list[i], list[i + gap]);
+            }
+        }
+
+        gap /= 2;
     }
 
-    gap /= 2;
-  }
-
-  bubbleSort(list, size);
+    bubbleSort(list, size);
 }
 
 #endif
